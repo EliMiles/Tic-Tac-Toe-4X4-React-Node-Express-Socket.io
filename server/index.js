bfbab344b6c8412721d28e3c43165e290752c450
Eli Miles <elimiles1987@gmail.com>
2019-09-20 20:35:00 +0300

    .

--- server/index.js
+++ server/index.js
@@ -1,11 +1,35 @@
 const app = require('http').createServer()
 const io = module.exports.io = require('socket.io')(app)
 
-const PORT = process.env.PORT || 3231
-
 const SocketManager = require('./SocketManager')
 
 io.on('connection', SocketManager)
+
+///////////////////////////////////////////////////////////////////
+
+// const express = require("express");
+
+// const http = require("http");
+
+// const socketIo = require("socket.io");
+
+// const SocketManager = require('./SocketManager');
+
+// const cors = require('cors');
+
+// const app = express();
+
+// app.use(cors());
+// app.options('*', cors());
+
+// const server = http.createServer(app);
+
+// const io = socketIo(server);
+
+// io.on('connection', SocketManager);
+
+//////////////////////////////////////////////////////////////////////
+const PORT = process.env.PORT || 3231;
 
 app.listen(PORT, () => {
     console.log('connected to port: ' + PORT);
