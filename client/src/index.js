3d18759ec8c4e2fc0d84948638a9afafba0c5494
Eli Miles <elimiles1987@gmail.com>
2019-09-20 18:42:31 +0300

    .

--- client/src/index.js
+++ client/src/index.js
@@ -7,22 +7,22 @@ import reduxThunk from 'redux-thunk';
 import AppRouter from './AppRouter';
 import reducers from './reducers.js';
 
-const store = createStore(
-    reducers,
-    {},
-    compose(
-        applyMiddleware(reduxThunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-); // store = Redux!
-
 // const store = createStore(
 //     reducers,
 //     {},
 //     compose(
-//         applyMiddleware(reduxThunk)
+//         applyMiddleware(reduxThunk),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 //     )
 // ); // store = Redux!
+
+const store = createStore(
+    reducers,
+    {},
+    compose(
+        applyMiddleware(reduxThunk)
+    )
+); // store = Redux!
 
 ReactDOM.render(
     <Provider store={store}><AppRouter /></Provider>, // Provider is the Glue between Redux & React
